92c8c04a64f665891a9da4baf6c2a66af89a5c81
CptShey <50450109+CptShey@users.noreply.github.com>
2019-05-09 14:54:15 +0200

    Update app.py
    change outputtext

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def hello():
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-    html = "<h3>Greet God {name}!</h3>" \
+    html = "<h3>Greet Gott {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
