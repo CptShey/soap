bf544134e9e97a5f80d103d388ae9be19ebee94d
CptShey <50450109+CptShey@users.noreply.github.com>
2019-05-10 23:26:04 +0200

    Update install_tomcat.sh

--- bash/install_tomcat.sh
+++ bash/install_tomcat.sh
@@ -7,4 +7,4 @@ sed -i -e 's/password="secret"/password="'$PASSWORD'"/g' /bash/tomcat-users.xml
 mv /bash/tomcat-users.xml /tomcat/conf
 mv /bash/tomcat_com /etc/init.d/tomcat
 chmod 755 /etc/init.d/tomcat
-update-rc.d tomcat defaults  80 01
+#update-rc.d tomcat defaults  80 01
