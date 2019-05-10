44f49e6ea25d81bce64a0115676aba00c42c4284
CptShey <50450109+CptShey@users.noreply.github.com>
2019-05-10 18:19:21 +0200

    Update install_tomcat.sh

--- bash/install_tomcat.sh
+++ bash/install_tomcat.sh
@@ -2,9 +2,9 @@
 
 cd /src
 tar -xzf tomcat-8.0.11.tar.gz
-mv /files/apache-tomcat-8.0.11 /tomcat
-sed -i -e 's/password="secret"/password="'$PASSWORD'"/g' /scripts/tomcat-users.xml
-mv /scripts/tomcat-users.xml /tomcat/conf
-mv /scripts/tomcat8 /etc/init.d/tomcat
+mv /src/apache-tomcat-8.0.11 /tomcat
+sed -i -e 's/password="secret"/password="'$PASSWORD'"/g' /bash/tomcat-users.xml
+mv /bash/tomcat-users.xml /tomcat/conf
+mv /bash/tomcat_com /etc/init.d/tomcat
 chmod 755 /etc/init.d/tomcat
 update-rc.d tomcat defaults  80 01
