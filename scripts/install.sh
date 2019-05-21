3bdb41306a269256f7a39139aa9295ef0dbefb38
CptShey <50450109+CptShey@users.noreply.github.com>
2019-05-21 20:44:17 +0200

    Create install.sh

---
+++ scripts/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "---------------------------------------------------- "
+echo "------------ INSTALL MAJOR DEPENDENCIES ------------ "
+apt-get update && apt-get install -y wget nano unzip default-jdk
+
+echo "---------------------------------------------------- "
+echo "------------------- INSTALL TOMCAT ----------------- "
+wget http://www-us.apache.org/dist/tomcat/tomcat-8/v8.5.40/bin/apache-tomcat-8.5.40.tar.gz
+tar -xzf apache-tomcat-8.5.40.tar.gz
+mv apache-tomcat-8.5.40 /usr/local/tomcat
+
+echo "---------------------------------------------------- "
+echo "------------------ MOVING FILES --------------------"
