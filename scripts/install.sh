fb72bde123f8db256730874b2e5f8104606bf3de
CptShey <50450109+CptShey@users.noreply.github.com>
2019-05-24 11:46:04 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@
 
 echo "---------------------------------------------------- "
 echo "------------ INSTALL MAJOR DEPENDENCIES ------------ "
-apt-get update && apt-get install -y wget nano unzip default-jdk
+apt-get update && apt-get install -y curl yum wget nano unzip default-jdk
 
 echo "---------------------------------------------------- "
 echo "------------------- INSTALL TOMCAT ----------------- "
@@ -11,14 +11,13 @@ tar -xzf apache-tomcat-8.5.40.tar.gz
 mv apache-tomcat-8.5.40 /usr/local/tomcat
 
 echo "---------------------------------------------------- "
-echo "------------------ MOVING FILES --------------------"
-
-echo "---------------------------------------------------- "
 echo "---------------- INSTALL ORACLE XE ------------------"
 
 curl -o oracle-database-preinstall-18c-1.0-1.el6.x86_64.rpm https://yum.oracle.com/repo/OracleLinux/OL6/latest/x86_64/getPackage/oracle-database-preinstall-18c-1.0-1.el6.x86_64.rpm
 yum -y localinstall oracle-database-preinstall-18c-1.0-1.el6.x86_64.rpm
 
 echo "---------------------------------------------------- "
-echo "--------------- END INSTALL ORACLE XE ---------------"
+echo "------------------ MOVING FILES --------------------"
+
+
 
