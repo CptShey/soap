c56dc537a818091a767796889cf4c4b998937593
CptShey <50450109+CptShey@users.noreply.github.com>
2019-05-31 13:31:15 +0200

    Delete install.sh

--- scripts/install.sh
+++
@@ -1,8 +0,0 @@
-
-echo "---------------------------------------------------- "
-echo "------------ INSTALL MAJOR DEPENDENCIES ------------ "
-apt-get update && apt-get install -y curl yum wget nano unzip default-jdk
-echo "------------ UPTDATE COMPLETE ------------ "
-
-
-./scripts/install_ssh.sh
