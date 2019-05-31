b1eb5eff935044732deb85435378b280652719dc
CptShey <50450109+CptShey@users.noreply.github.com>
2019-05-31 13:29:58 +0200

    Update install_oraclexe18c.sh

--- scripts/install_oraclexe18c.sh
+++ scripts/install_oraclexe18c.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-echo "---------------------------------------------------- "
-echo "------------ INSTALL MAJOR DEPENDENCIES ------------ "
-yum install glibc.*
+echo "========================================================================="
+echo "=                      INSTALL MAJOR DEPENDENCIES                       ="
+echo "========================================================================="
+curl -o oracle-database-preinstall-18c-1.0-1.el7.x86_64.rpm https://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/getPackage/oracle-database-preinstall-18c-1.0-1.el7.x86_64.rpm
+yum -y localinstall oracle-database-preinstall-18c-1.0-1.el7.x86_64.rpm
+echo "========================================================================="
+echo "=                      INSTALL ORACLE XE 18c                            ="
+echo "========================================================================="
+yum -y localinstall files/oracle-database-xe-18c-1.0-1.x86_64.rpm
+echo "========================================================================="
+echo "=                        SETUP ORACLE XE 18c                            ="
+echo "========================================================================="
+printf $PASSWORD\\n$PASSWORD\\n | /etc/init.d/oracle-xe-18c configure
+
 
