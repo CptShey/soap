fa5299bb94133eb19c67dacc8aba6b897e685b00
CptShey <50450109+CptShey@users.noreply.github.com>
2019-08-25 09:57:43 +0200

    changed url
    REASON: ddns service expired

--- scripts/dload.sh
+++ scripts/dload.sh
@@ -4,11 +4,11 @@ cd files
 echo "========================================================================="
 echo "=                          DOWNLOAD APEX                                ="
 echo "========================================================================="
-curl -O --progress-bar http://digitalreaktor.ddns.net/odbs/apex.zip
+curl -O --progress-bar http://www.svujo.com/odbs/apex.zip
 echo "========================================================================="
 echo "=                          DOWNLOAD ORACLE XE 18c                       ="
 echo "========================================================================="
-curl -O --progress-bar http://digitalreaktor.ddns.net/odbs/oracle-database-xe-18c-1.0-1.x86_64.rpm
+curl -O --progress-bar http://www.svujo.com/odbs/oracle-database-xe-18c-1.0-1.x86_64.rpm
 
 echo "========================================================================="
 echo "=                         CREATE DEBUGLOGS FOLER                        ="
